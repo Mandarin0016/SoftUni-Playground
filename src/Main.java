71dd6c45fce462b580fac4c4db2b80cb57c6105a
Viktor Aleksandrov <viktorostrov07@gmail.com>
2023-02-11 18:37:55 +0200

    formated

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello mandarine!");
-	int a = 5 + 5;
-	int b = 5 + 5;
+        int a = 5 + 5;
+        int b = 5 + 5;
     }
 }
