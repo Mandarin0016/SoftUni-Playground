226d5befc952bf45075415606b54d725bb66580f
Petyo Veselinov <borovaneca@virgilio.it>
2023-02-12 18:20:57 +0200

    Added new line

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         System.out.println("Hello mandarine!");
         int a = 5 + 5;
         int b = 5 + 5;
+        System.out.println("Hello");
         System.out.println("Test3");
     }
 }
