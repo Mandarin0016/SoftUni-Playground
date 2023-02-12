99b0fef848a1cb6912dd0945a0e5b8d0666f2232
Petyo Veselinov <borovaneca@virgilio.it>
2023-02-12 18:46:59 +0200

    updated

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
         System.out.println("Hello mandarine!");
         int a = 5 + 5;
         int p = 1 + 1;
+        System.out.println("Here is the new line!");
+        System.out.println("Here is the new line!");
+        System.out.println("Here is the new line!");
         int b = 5 + 5;
         System.out.println("Test3");
         boolean c = true;
