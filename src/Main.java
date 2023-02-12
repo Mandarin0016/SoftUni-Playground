bdbee55b7f3447d15a09dc241238f4bbcf1c079d
Petyo Veselinov <borovaneca@virgilio.it>
2023-02-12 18:15:34 +0200

    My change

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello guys!");
         System.out.println("Hello mandarine!");
         int a = 5 + 5;
         int b = 5 + 5;
